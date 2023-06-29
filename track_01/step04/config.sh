4eae5e9930b9f002841878e4ca22ebe8e47fd85a
Taewan Kim <taewanme@gmail.com>
2023-06-30 07:47:14 +0900

    Update config.sh

--- track_01/step04/config.sh
+++ track_01/step04/config.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-fn config function dpws1_pipeline_app converting_file_fn bucket_name dpws1_taewan.kim_converted_file
+fn config function dpws1_pipeline_app converting_file_fn bucket_name <<#Please Update #RES#04 value in Excel File>>
 
 
