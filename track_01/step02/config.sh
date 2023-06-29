b855179d9c6ce18a1f5f922a1ca720dc3381c04a
Taewan Kim <taewanme@gmail.com>
2023-06-30 04:11:16 +0900

    Update config.sh

--- track_01/step02/config.sh
+++ track_01/step02/config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-fn config function dpws-pipeline enhancement_fn bootstrap.servers cell-1.streaming.ap-tokyo-1.oci.oraclecloud.com:9092
-fn config function dpws-pipeline enhancement_fn sasl.username apackrsct/taewanme@gmail.com/ocid1.streampool.oc1.ap-tokyo-1.amaaaaaavsea7yiatut7gbr7bgrciy3xzkea5ttbjex5cq5irbgaswrtdefq
-fn config function dpws-pipeline enhancement_fn sasl.password r7oCKgz3wE[q2BB#R#W9
+fn config function dpws-pipeline enhancement_fn bootstrap.servers <<PLEASE Replace With #RES#INFO#03>>
+fn config function dpws-pipeline enhancement_fn sasl.username <<PLEASE Replace With #RES#INFO#04>>
+fn config function dpws-pipeline enhancement_fn sasl.password <<PLEASE Replace With #RES#INFO#05>>
 fn config function dpws-pipeline enhancement_fn topic enhanced_stream
 
