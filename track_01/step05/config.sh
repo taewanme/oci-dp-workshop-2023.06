b7de6382212321fe9a1b7771f721d55a755bb59f
Taewan Kim <taewanme@gmail.com>
2023-06-30 09:52:12 +0900

    Update config.sh

--- track_01/step05/config.sh
+++ track_01/step05/config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-fn config function dpws-pipeline triggering_fn ords-base-url <<##RES#INFO#09>>
-fn config function dpws-pipeline triggering_fn dbuser <<##RES#INFO#07>>
-fn config function dpws-pipeline triggering_fn password <<##RES#INFO#08>>
-fn config function dpws-pipeline triggering_fn base-objectstorage-url <<##RES#INFO#10>>
+fn config function dpws1_pipeline_app triggering_fn ords-base-url <<##RES#INFO#09>>
+fn config function dpws1_pipeline_app triggering_fn dbuser <<##RES#INFO#07>>
+fn config function dpws1_pipeline_app triggering_fn password <<##RES#INFO#08>>
+fn config function dpws1_pipeline_app triggering_fn base-objectstorage-url <<##RES#INFO#10>>
