27cb64a3e098ca0178f0ee9e95ceed754bca3d17
Taewan Kim <taewanme@gmail.com>
2023-08-01 17:59:34 +0900

    Update config.sh

--- track_01/step02/config.sh
+++ track_01/step02/config.sh
@@ -2,7 +2,7 @@
 
 fn config function dpws1_pipeline_app enhancement_fn bootstrap.servers <<PLEASE Replace With #RES#INFO#03>>
 fn config function dpws1_pipeline_app enhancement_fn sasl.username <<PLEASE Replace With #RES#INFO#04>>
-fn config function dpws1_pipeline_app enhancement_fn sasl.password <<PLEASE Replace With #RES#INFO#05>>
+fn config function dpws1_pipeline_app enhancement_fn sasl.password '<<PLEASE Replace With #RES#INFO#11>>'
 fn config function dpws1_pipeline_app enhancement_fn topic enhanced_stream
 fn config function dpws1_pipeline_app enhancement_fn compartmentocid <<PLEASE Replace With COMPARTMENT OCID in Excel File>>
 
